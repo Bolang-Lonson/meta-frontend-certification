22c63b5f9a72e6b398eec76d5c8e1fe4ae6d99f6
Bolang-Lonson <bbeinyoh12@gmail.com>
2023-12-08 23:15:51 -0800

    ...

---
+++ Programming With JavaScript/Week 1/loops.js
@@ -0,0 +1,13 @@
+// nested loop
+for(var i = 1; i <= 2; i++){
+    for(var j = 1; j <= 5; j++){
+        console.log(`Week ${i} day ${j}`);
+    }
+}
+
+for( var year = 2023; year < 2025; year++){
+    console.log(year);
+    for(var month = 6; month < 9; month++){
+        console.log("----------", month);
+    }
+}
