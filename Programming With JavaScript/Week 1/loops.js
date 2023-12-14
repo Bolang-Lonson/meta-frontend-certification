8b975222f91d9dd8f97d55d85056bd03a13e1cf1
Bolang-Lonson <bbeinyoh12@gmail.com>
2023-12-14 06:29:14 +0100

    ...

--- Programming With JavaScript/Week 1/loops.js
+++ Programming With JavaScript/Week 1/loops.js
@@ -11,3 +11,18 @@ for( var year = 2023; year < 2025; year++){
         console.log("----------", month);
     }
 }
+
+// Multiplication table
+for(let i = 1; i < 10; i++){
+    for(var j = 1; j < 10; j++){
+        console.log(`${i} times ${j} equals ${i * j}`);
+    }
+    console.log('*******************');
+}
+
+// Custom division table
+for(var i = 100; i > 10; i -= 10){
+    for(var j = 10; j > 4; j -= 5){
+        console.log(`${i} divided by ${j} equals ${i / j}`);
+    }
+}
