462c6a100d66108f571ad3db16e43c32a7a72edf
Bolang-Lonson <bbeinyoh12@gmail.com>
2023-12-15 09:19:18 +0100

    array and objects exercise

---
+++ Programming With JavaScript/Week 2/ex1.js
@@ -0,0 +1,18 @@
+var clothes = [];
+// Adding my 5 favorite clothes
+clothes.push('Ripped jeans');
+clothes.push('White T');
+clothes.push('Black T');
+clothes.push('Denim jacket');
+clothes.push('Sweater');
+
+// removing 5th piece
+clothes.pop();
+
+clothes.push('Turtleneck');
+console.log(clothes[2]);
+
+let favCar = {};
+favCar.color = 'Black'; // Giving it my preferred color attribute
+favCar.convertible = false;
+console.log(favCar);
