9a70e8f1428f7c07425fc830e19ddd2e1a41d70a
Bolang-Lonson <bbeinyoh12@gmail.com>
2024-01-15 12:19:26 +0100

    added a second test

--- Programming With JavaScript/Week 4/addFive.test.js
+++ Programming With JavaScript/Week 4/addFive.test.js
@@ -3,4 +3,7 @@ const addFive = require('./addFive');
 
 test('returns the number plus 5', () => {
     expect(addFive(1)).toBe(6);
-})
+});
+test('returns the number plus 5', () => {
+    expect(addFive(5)).toBe(10);
+});
